94593815bf443ee593eca0c548921a2090437991
WZJYZ <liwenjie_0121@163.com>
2018-12-19 19:37:05 +0800

    Initial commit

---
+++ 02-Quick-Find/UnionFind1.h
@@ -0,0 +1,18 @@
+//
+// Created by lwj on 2018/12/19.
+//
+
+#ifndef UNION_FIND_UNIONFIND1_H
+#define UNION_FIND_UNIONFIND1_H
+
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+namespace UF1{
+    class UnionFind{
+
+    };
+}
+
+#endif //UNION_FIND_UNIONFIND1_H
